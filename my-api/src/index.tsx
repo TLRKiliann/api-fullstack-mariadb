8cfe70f4ada560be46a1ed120f7c382c32ca6d2d
TLRKiliann <philogenie@protonmail.com>
2022-10-20 13:09:15 +0200

    setup index for react-router-dom & AuthProvider (Context)

--- my-api/src/index.tsx
+++ my-api/src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from "./Context/AuthProvider";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,7 +11,11 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
